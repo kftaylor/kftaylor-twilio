c1959975e29208d5aa20b3108a77014f92d8a33c
Victor Ivanov <vic.ivanoff@gmail.com>
2014-12-23 00:55:36 +0300

    don't reply anything if no code present

--- app.rb
+++ app.rb
@@ -15,14 +15,13 @@ post '/sms' do
   sms_body = params[:Body]
   sms_body = sms_body.gsub("/[^A-Za-z0-9]/u", ' ').strip.downcase
 
-  # sender = params[:From]
-  # friends = {
-  #     "+14153334444" => "Curious George",
-  #     "+14158157775" => "Boots",
-  #     "+14155551234" => "Virgil"
-  # }
-  twiml = Twilio::TwiML::Response.new do |r|
-    r.Message "Hello, you sent #{sms_body}."
+  if sms_body == 'summit'
+    twiml = Twilio::TwiML::Response.new do |r|
+      r.Message "Hello, you sent #{sms_body}."
+    end
+  else
+    twiml = Twilio::TwiML::Response.new
   end
+
   twiml.text
 end
